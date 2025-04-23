aaab2b2cc1dd13f79c4d075f19a566d6457e650f
gourab8389 <deyg6988@gmail.com>
2025-04-23 18:25:48 +0530

    Add nodemailer transport setup and update dependencies for email functionality

--- src/api/send-mail/route.ts
+++ src/api/send-mail/route.ts
@@ -1,11 +1,31 @@
-import { Request, Response, Router } from 'express';
-import { sendSuccess } from '../../utils/response-handler';
+import { Request, Response, Router } from "express";
+import { transporter } from "../../lib/nodemailer";
+import { z } from "zod"
 
 const router = Router();
 
-// GET /api/send-mail
-router.get('/', (req: Request, res: Response) => {
-  sendSuccess(res, { message: 'Hello World!' }, 'Greeting retrieved successfully');
+const emailOption = z.object({
+  email: z.string().email(),
+  otp: z.string().length(6),
+})
+
+router.post("/send-mail", async (req: Request, res: Response) => {
+
+  const { email, otp } = emailOption.parse(req.body)
+  const mailOptions = {
+    from: process.env.GMAIL_EMAIL,
+    to: email,
+    subject: "OTP for Email Verification",
+    text: `Your OTP is ${otp}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ message: "Error sending email" });
+  }
 });
 
 export default router;
